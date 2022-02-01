24c4f7c36b3bae0bd7e47cc153afad7f05f4c9bb
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-01-31 22:19:19 -0300

    Adicionando Eventos a Porta 01

--- src/components/Porta.tsx
+++ src/components/Porta.tsx
@@ -2,12 +2,12 @@ import PortaModel from "../../model/Porta"
 import styles from "../styles/Porta.module.css"
 
 interface PortaProps{
-    porta: PortaModel
+    value: PortaModel
 }
 
 export default function Porta(props: PortaProps) {
     
-    const {porta} = props
+    const porta = props.value
 
     const selecionada = porta.selecionada ? styles.selecionada : ''
     
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   
   return (
     <div style={{display:"flex"}}>
-      <Porta  porta={p1}/>
+      <Porta  value={p1}/>
     </div>
   )
 }
