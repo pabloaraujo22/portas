1fa66bcc985f5ef19e61166c86bccac06f977f2b
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-02-03 22:03:47 -0300

    Lidando com Array de Portas #02

--- functions/portas.ts
+++ functions/portas.ts
@@ -1,10 +1,22 @@
 import PortaModel from "../model/Porta"
 
-export default function criarPortas(qtde:number,selecionada:number) : PortaModel[] {
+export  function criarPortas(qtde:number,selecionada:number) : PortaModel[] {
     return(Array.from({length: qtde}, (_,i)=>{
         const numero = i + 1
         const temPresente = numero === selecionada
 
         return new PortaModel(numero,temPresente)
     }))
+}
+
+export function atualizarPortas(portas: PortaModel[], portaModificada: PortaModel) : PortaModel[] {
+    return portas.map(portaAtual=>{
+        const mesmaPorta = portaAtual.numero === portaModificada.numero
+
+        if(mesmaPorta){
+            return portaModificada
+        }else{
+            return portaModificada.aberta ? portaAtual : portaAtual.desSelecionar()
+        }
+    })
 }
