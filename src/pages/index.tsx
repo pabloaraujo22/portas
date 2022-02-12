153c9c741b53ad7168e37e543b83bf63a71d1788
pabloaraujo22 <66523165+pabloaraujo22@users.noreply.github.com>
2022-02-12 14:25:40 -0300

    Estruturando Formulario

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
-    return (
-        <div>
-            <h1> Fomulário de Inicio </h1>
-        </div>
-    );
+/* eslint-disable @next/next/link-passhref */
+import Link from 'next/link';
+import Cartao from '../components/Cartao';
+import styles from '../styles/Fomulario.module.css';
+export default function Formulario() {
+  return (
+    <div className={styles.formulario}>
+      <div className="">
+        <Cartao bgColor="#c0392c">
+          <h1>Matty Hall</h1>
+        </Cartao>
+        <Cartao></Cartao>
+      </div>
+      <div>
+        <Cartao></Cartao>
+        <Cartao bgColor="#28a085">
+          <Link href={`/jogo/4/2`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
+    </div>
+  );
 }
